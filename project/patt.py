097e7349ab7c816b993dd5c42d8b94cc22107683
Kuanysh <107105973+kuanyshbakytuly@users.noreply.github.com>
2022-10-12 13:35:54 +0600

    Delete patt.py

--- project/patt.py
+++
@@ -1,6 +0,0 @@
-print(len({'English': 'en_US', 'Arabic': 'ar_AE', 'Chinese - Simplified': 'zh_CN',
-                     'Chinese - Traditional': 'zh_TW', 'Czech': 'cs_CZ', 'Danish': 'da_DK', 'Indonesian': 'in_ID',
-                     'Malaysian': 'ms_MY', 'Dutch': 'nl_NL', 'French': 'fr_FR', 'Finnish': 'fi_FI', 'German': 'de_DE',
-                     'Italian': 'it_IT', 'Japanese': 'ja_JP', 'Korean': 'ko_KR', 'Norwegian': 'no_NO',
-                     'Polish': 'pl_PL', 'Portuguese': 'pt_BR', 'Romanian': 'ro_RO', 'Russian': 'ru_RU',
-                     'Spanish': 'es_ES', 'Swedish': 'sv_SE', 'Thai': 'th_TH', 'Filipino': 'tl_PH', 'Turkish': 'tr_TR'}))
